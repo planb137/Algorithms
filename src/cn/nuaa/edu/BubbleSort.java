55a0844d967bd17ab3e660fb5878ed0a391fdeab
planb137 <qiujj@nuaa.edu.cn>
2020-05-13 19:49:37 +0800

    Initial commit

---
+++ src/cn/nuaa/edu/BubbleSort.java
@@ -0,0 +1,28 @@
+package cn.nuaa.edu;
+
+import java.util.Arrays;
+
+/**
+ * @author planb
+ * @date 2020/5/13 16:14
+ * 备注：
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int []arr = {1,8,12,6,5,34,3,57,2};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void  sort(int arr[]){
+        for (int i =0; i< arr.length - 1;i++) {
+            for (int j = 0;j< arr.length - i -1 ;j++){
+                if (arr[j]>arr[j+1]){
+                    int temp  = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+}
