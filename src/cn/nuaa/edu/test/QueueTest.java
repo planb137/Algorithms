78ec117b4adb9d77c801d17865931c70eb6dbdeb
planb137 <qiujj@nuaa.edu.cn>
2020-06-18 16:22:51 +0800

    ConstructBinaryTree.java && QueueTest.java

--- src/cn/nuaa/edu/test/QueueTest.java
+++ src/cn/nuaa/edu/test/QueueTest.java
@@ -9,12 +9,13 @@ import java.util.LinkedList;
  */
 public class QueueTest {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        list.addFirst(1);
-        list.addFirst(2);
-        System.out.println(list.getFirst());
-        System.out.println(list.removeFirst());
-        System.out.println(list.toString());
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+
+        System.out.println(queue.poll());
+        System.out.println(queue.toString());
 
     }
 }
