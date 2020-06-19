e8d7174a60fa8c3cee107f991dac76e267d6abae
planb137 <qiujj@nuaa.edu.cn>
2020-06-19 22:47:14 +0800

    test

--- src/cn/nuaa/edu/test/Test.java
+++ src/cn/nuaa/edu/test/Test.java
@@ -7,6 +7,9 @@ package cn.nuaa.edu.test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println((char)97);
+       // System.out.println((char)97);
+       // String[] arr= new String[Integer.MAX_VALUE];(溢出)
+        System.out.println(8&8);
+        System.out.println(String.valueOf(Integer.toBinaryString(11)));
     }
 }
