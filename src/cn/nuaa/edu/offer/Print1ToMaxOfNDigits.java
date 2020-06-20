9a352b325bd54e93c522e5fa59e9f41ccf34eced
planb137 <qiujj@nuaa.edu.cn>
2020-06-20 13:45:19 +0800

    Print1ToMaxOfNDigits

---
+++ src/cn/nuaa/edu/offer/Print1ToMaxOfNDigits.java
@@ -0,0 +1,45 @@
+package cn.nuaa.edu.offer;
+
+/**
+ * @author planb
+ * @date 2020/6/20 13:27
+ * 备注：
+ */
+public class Print1ToMaxOfNDigits {
+    public static void print1ToMaxOfNDights1s(int n) {
+        if(n<=0) {
+            return;
+        }
+        char[] digit = new char[n];
+        for(int i=0;i<n;i++) {
+            digit[i] = '0';
+        }
+        for(int i=n-1;i>=0;i--) {
+            while(digit[i]!='9') {
+                int m=0;
+                digit[m]++;
+                while(m<n-1 && digit[m]>'9') {
+                    digit[m]='0';
+                    digit[m+1]++;
+                    m++;
+                }
+                printdigits(digit);
+            }
+        }
+    }
+
+    private static void printdigits(char[] digit) {
+        int m = digit.length-1;
+        while(digit[m]=='0') {
+            m--;
+        }
+        for(int i=m;i>=0;i--) {
+            System.out.print(digit[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        print1ToMaxOfNDights1s(2);
+    }
+}
