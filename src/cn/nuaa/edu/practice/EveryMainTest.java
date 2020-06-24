2163ea027643b0b92f401ac0d680090b785fd23b
planb137 <qiujj@nuaa.edu.cn>
2020-06-24 21:46:05 +0800

    EveryMainTest

---
+++ src/cn/nuaa/edu/practice/EveryMainTest.java
@@ -0,0 +1,15 @@
+package cn.nuaa.edu.practice;
+
+/**
+ * @author planb
+ * @date 2020/6/24 21:32
+ * 备注：
+ */
+public class EveryMainTest {
+    public static void main(String[] args) {
+        int[] nums = {-1,2,3,-4};
+        int target = 4;
+        ThreeSumClosest threeSumClosest = new ThreeSumClosest();
+        System.out.println(threeSumClosest.threeSumClosest(nums,target));
+    }
+}
