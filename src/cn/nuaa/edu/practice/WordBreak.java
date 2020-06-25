f80b6666aa1c45f8c78081ae0b13988a6bb5b0a0
planb137 <qiujj@nuaa.edu.cn>
2020-06-25 17:10:30 +0800

    WordBreak

---
+++ src/cn/nuaa/edu/practice/WordBreak.java
@@ -0,0 +1,33 @@
+package cn.nuaa.edu.practice;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author planb
+ * @date 2020/6/25 16:48
+ * 备注：
+ */
+public class WordBreak {
+    HashMap<String,Boolean> map =  new HashMap<String,Boolean>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+            boolean[] dp = new boolean[s.length()+1];
+            for (String s1 : wordDict){
+                map.put(s1,true);
+            }
+            dp[0] = true;
+            for(int i = 1;i<s.length();i++){
+                for (int j=i-1;j<s.length();j++){
+                    dp[i] =(dp[j] && check(s.substring(j,i)));
+                    if (dp[i]){
+                        break;
+                    }
+                }
+            }
+            return dp[s.length()];
+    }
+
+    public boolean check(String s){
+        return map.getOrDefault(s,false);
+    }
+}
