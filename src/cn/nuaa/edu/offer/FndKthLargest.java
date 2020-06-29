1d2223a6ce35700a6d05a443bb17829ce6d6ab7b
planb137 <qiujj@nuaa.edu.cn>
2020-06-29 14:45:44 +0800

    FndKthLargest

--- src/cn/nuaa/edu/offer/FndKthLargest.java
+++ src/cn/nuaa/edu/offer/FndKthLargest.java
@@ -9,16 +9,19 @@ import java.util.Random;
  */
 public class FndKthLargest {
     Random random = new Random();
+    public static int index;
+
     public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+        index = nums.length - k;
+        return quickSelect(nums, 0, nums.length - 1);
     }
 
-    public int quickSelect(int[] a, int l, int r, int index) {
+    public int quickSelect(int[] a, int l, int r) {
         int q = randomPartition(a, l, r);
         if (q == index) {
             return a[q];
         } else {
-            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
+            return q < index ? quickSelect(a, q + 1, r) : quickSelect(a, l, q - 1);
         }
     }
 
