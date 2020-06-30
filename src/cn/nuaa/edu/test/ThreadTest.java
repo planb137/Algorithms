18ecf2f02ffddf911dd26041c2e74cdd42c3aba4
planb137 <qiujj@nuaa.edu.cn>
2020-06-30 15:52:08 +0800

    ThreadTest

--- src/cn/nuaa/edu/test/ThreadTest.java
+++ src/cn/nuaa/edu/test/ThreadTest.java
@@ -2,6 +2,7 @@ package cn.nuaa.edu.test;
 
 import cn.nuaa.edu.practice.QuickSortPartition;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -10,33 +11,43 @@ import java.util.concurrent.ExecutionException;
  * 备注：
  */
 public class ThreadTest extends Thread {
-    public ThreadTest(String name){
+    public double startTime;
+    public ThreadTest(String name,double startTime){
         super(name);
+        this.startTime = startTime;
     }
+
     @Override
     public void run() {
         while (true){
-            int[] nums = {-1,2,3,-4,64,23,1,57,35,242,57,2,57,34,57};
-            long start,end;
-            start = System.currentTimeMillis();
+            int[] nums = new int[10000000];
+            for(int i =0 ;i<10000000;i++){
+                nums[i] = new Random().nextInt(1000000);
+            }
             //Arrays.sort(nums);
             int[] x = QuickSortPartition.qsort(nums,0,nums.length-1);
-            try {
-                sleep(1000);//休息一秒
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            end = System.currentTimeMillis();
-            System.out.println( "当前的线程为： "+getName()+" 本次执行花费了:" + (end - start) + "(ms)");
+           /* System.out.print("排序的结果为： ");
+            for (int temp:x){
+                System.out.print(temp+" ");
+            }*/
+//            try {
+//                sleep(1000);//休息一秒
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            System.out.println( "当前的线程为： "+getName()+
+                    " 从创建到执行花费了:" + (System.currentTimeMillis()- this.startTime) + "(ms)");
             break;
         }
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException  {
-        ThreadTest[] threadTest = new ThreadTest[10];
-        for (int i =0 ;i<10;i++){
-            threadTest[i] = new ThreadTest(Integer.toString(i));
-            threadTest[i].run();
+        ThreadTest[] threadTest = new ThreadTest[100];
+        for (int i =0 ;i<threadTest.length;i++){
+            threadTest[i] = new ThreadTest(Integer.toString(i),System.currentTimeMillis());
+        }
+        for(int j =0; j<threadTest.length;j++){
+            threadTest[j].run();
         }
 
     }
