a60fc40dc4786f86392c43c2e52b914060b49dd0
planb137 <qiujj@nuaa.edu.cn>
2020-07-02 17:04:16 +0800

    CountDownLatchDemo && CycleBarrierDemo

---
+++ src/cn/nuaa/edu/juc/CountDownLatchDemo.java
@@ -0,0 +1,36 @@
+package cn.nuaa.edu.juc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author planb
+ * @date 2020/7/2 16:29
+ * 备注：
+ */
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch downLatch = new CountDownLatch(6);
+
+        for (int i = 0; i < 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName()+"\t离开教室");
+                downLatch.countDown();
+            }).start();
+        }
+        downLatch.await();
+        System.out.println(Thread.currentThread().getName()+"\t班长关门走人");
+    }
+
+    public static void closeDoor() {
+        for (int i = 0; i < 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName()+"\t离开教室");
+            }).start();
+        }
+        System.out.println(Thread.currentThread().getName()+"\t班长关门走人");
+    }
+}
---
+++ src/cn/nuaa/edu/juc/CycleBarrierDemo.java
@@ -0,0 +1,31 @@
+package cn.nuaa.edu.juc;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author planb
+ * @date 2020/7/2 16:49
+ * 备注：
+ */
+public class CycleBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7,()->{
+            System.out.println("人数达到！开始开会");
+        });
+        for (int i = 0; i < 7; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName()+"\t到达了第:"+tempInt+"人");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            },String.valueOf(i)).start();
+        }
+
+    }
+}
