68f3b3b3208ec89bd2528236bebb0c750c790715
planb137 <qiujj@nuaa.edu.cn>
2020-07-02 19:30:25 +0800

    SemaphoreDemo

---
+++ src/cn/nuaa/edu/juc/SemaphoreDemo.java
@@ -0,0 +1,34 @@
+package cn.nuaa.edu.juc;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author planb
+ * @date 2020/7/2 17:04
+ * 备注：抢车位，控制多线程的并发数
+ */
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        //模拟资源类，有3个空车位
+        Semaphore semaphore = new Semaphore(1);
+        for (int i = 0; i < 6; i++) {
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName()+"\t抢占了车位");
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+"\t离开了车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
