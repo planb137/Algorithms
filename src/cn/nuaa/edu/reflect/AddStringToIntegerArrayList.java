41e1c738ec5cfc96fde26bf03410af4dc1947af8
planb137 <qiujj@nuaa.edu.cn>
2020-07-06 10:43:46 +0800

    AddStringToIntegerArrayList 反射越过泛型检查：像ArrayList<Integer>中添加String

---
+++ src/cn/nuaa/edu/reflect/AddStringToIntegerArrayList.java
@@ -0,0 +1,29 @@
+package cn.nuaa.edu.reflect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+/**
+ * @author planb
+ * @date 2020/7/6 10:09
+ * 备注：反射越过泛型检查：向ArrayList<Integer>中添加String
+ */
+public class AddStringToIntegerArrayList {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(10);
+        integers.add(20);
+
+        Class<? extends ArrayList> c = integers.getClass();
+        Method add = c.getMethod("add", Object.class);
+        add.invoke(integers, 1);
+        add.invoke(integers, "hello");
+        add.invoke(integers, "world");
+        System.out.println(integers);
+
+
+
+
+    }
+}
