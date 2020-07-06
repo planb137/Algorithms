d6b48d3928ef310719503db7900ba25f448c1a90
planb137 <qiujj@nuaa.edu.cn>
2020-07-06 10:57:29 +0800

    ReflectDemo

--- src/cn/nuaa/edu/reflect/ReflectDemo.java
+++ src/cn/nuaa/edu/reflect/ReflectDemo.java
@@ -18,12 +18,13 @@ public class ReflectDemo {
         System.out.println(s.getClass()==c1);
         Class<?> c3 = Class.forName("cn.nuaa.edu.reflect.Student");
         System.out.println(c3 ==c1);*/
-/*        Constructor<?>[] constructors = c1.getConstructors();
+
+       Constructor<?>[] constructors = c1.getConstructors();
         Constructor<?>[] declaredConstructors = c1.getDeclaredConstructors();
         for (Constructor con:declaredConstructors) {
             System.out.println(con);
-        }*/
-   /*     Constructor<Student> constructor = c1.getConstructor();
+        }
+       Constructor<Student> constructor = c1.getConstructor();
         Student student = constructor.newInstance();
         System.out.println(student);//Student{name='null', age=0, addrss='null'}
 
@@ -31,35 +32,35 @@ public class ReflectDemo {
         Student student1 = constructor1.newInstance("lisi", 12, "nanjing");
         System.out.println(student1);//Student{name='lisi', age=12, addrss='nanjing'}*/
 
-        /*Constructor<?> constructor2 = c1.getDeclaredConstructor(String.class);
+        Constructor<?> constructor2 = c1.getDeclaredConstructor(String.class);
         constructor2.setAccessible(true);//私有构造方法，取消访问检查，暴力反射
         Object zhangsan = constructor2.newInstance("zhangsan ");
         System.out.println(zhangsan);//Student{name='zhangsan ', age=0, addrss='null'}
-*/
-/*        Field[] fields = c1.getFields();
+
+       Field[] fields = c1.getFields();
         Field[] fields2 = c1.getDeclaredFields();
         for (Field f: fields2) {
             System.out.println(f);
-        }*/
+        }
 
-        /*Constructor<Student> constructor = c1.getConstructor();
-        Student student = constructor.newInstance();
+        Constructor<Student> constructor5 = c1.getConstructor();
+        Student student2 = constructor5.newInstance();
         Field address = c1.getField("addrss");
-        address.set(student, "南京");//给obj的成员变量address赋值南京
-        System.out.println(student); //Student{name='null', age=0, addrss='南京'}
-*/
-        /*Constructor<Student> constructor1 = c1.getConstructor();
-        Student student1 = constructor1.newInstance();
+        address.set(student2, "南京");//给obj的成员变量address赋值南京
+        System.out.println(student2); //Student{name='null', age=0, addrss='南京'}
+
+        Constructor<Student> constructor3 = c1.getConstructor();
+        Student student3 = constructor3.newInstance();
         Field name = c1.getDeclaredField("name");
         name.setAccessible(true);
         name.set(student1,"planb");
-        System.out.println(student1);//Student{name='planb', age=0, addrss='null'}
-*/
+        System.out.println(student3);//Student{name='planb', age=0, addrss='null'}
+
         Method declaredMethods = c1.getDeclaredMethod("method3", String.class, int.class);
         declaredMethods.setAccessible(true);
-        Constructor<Student> constructor = c1.getConstructor();
-        Student student = constructor.newInstance();
-        Object invoke = declaredMethods.invoke(student, "4",8);
+        Constructor<Student> constructor4 = c1.getConstructor();
+        Student student4 = constructor4.newInstance();
+        Object invoke = declaredMethods.invoke(student4, "4",8);
         System.out.println(invoke);
     }
 }
