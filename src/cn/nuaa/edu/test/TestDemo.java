aee995855cddd251ee30369c47b56d26266dd3f7
planb137 <qiujj@nuaa.edu.cn>
2020-07-10 21:09:02 +0800

    ABADemo 使用AtomicStampReference解决ABA问题

--- src/cn/nuaa/edu/test/TestDemo.java
+++ src/cn/nuaa/edu/test/TestDemo.java
@@ -1,7 +1,11 @@
 package cn.nuaa.edu.test;
 
-import java.util.Arrays;
-import java.util.List;
+import cn.nuaa.edu.juc.MyThreadPoolDemo;
+import com.sun.tools.classfile.ConstantPool;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.*;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author planb
@@ -12,10 +16,25 @@ public class TestDemo {
     public static void main(String[] args) {
         Integer[] arr = {1,4,5,7,8};
         List<Integer> list = Arrays.asList(arr);
-        System.out.println(list);
+/*        System.out.println(list);
         arr[0]=12;
         list.add(123113);
-        System.out.println(list);
+        System.out.println(list);*/
+        ArrayList<Integer> integers = new ArrayList<>(5);
+        ArrayList<Integer> integers2 = new ArrayList<>(5);
+
+        MyThreadPoolDemo myThreadPoolDemo = new MyThreadPoolDemo();
+        if(myThreadPoolDemo instanceof MyThreadPoolDemo ){
+            System.out.println("yes");
+        }
+
+        Integer a = new Integer(2);
+        System.out.println(a.hashCode());
+        if(a instanceof Integer){
+            System.out.println("yes");
+        }
+        System.out.println(integers.hashCode());
+        System.out.println(integers2.hashCode());
 
     }
 }
