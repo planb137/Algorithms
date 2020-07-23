93f513d2777b18a184762a112b42759fd61c8bf5
planb137 <qiujj@nuaa.edu.cn>
2020-07-23 16:47:14 +0800

    推荐使用解法二

--- src/cn/nuaa/edu/leetcode/Offer12.java
+++ src/cn/nuaa/edu/leetcode/Offer12.java
@@ -9,6 +9,7 @@ import java.util.List;
  * 备注：剑指offer12，矩阵路径
  */
 public class Offer12 {
+    //解法一：
     public boolean exist(char[][] p, String word) {
         boolean[] res = new boolean[1];
         boolean[][] pb = new boolean[p.length][p[0].length];
@@ -61,4 +62,30 @@ public class Offer12 {
         }
         return loc;
     }
+
+    //解法二：推荐
+    public boolean exist2(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs2(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    boolean dfs2(char[][] board, char[] word, int i, int j, int k) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k])
+        { return false;}
+        if (k == word.length - 1) { return true;}
+
+          char tmp = board[i][j];
+          board[i][j] = '/';
+          boolean res = dfs2(board, word, i + 1, j, k + 1) || dfs2(board, word, i - 1, j, k + 1) ||
+                        dfs2(board, word, i, j + 1, k + 1) || dfs2(board, word, i, j - 1, k + 1);
+          board[i][j] = tmp;
+           return res;
+            }
+
 }
