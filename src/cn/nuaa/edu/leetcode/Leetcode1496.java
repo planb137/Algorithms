8d89bec259cb53d531a4c65f4fb593f19bf9e980
planb137 <qiujj@nuaa.edu.cn>
2020-07-30 20:23:38 +0800

    leetcode 1496. 判断路径是否相交

---
+++ src/cn/nuaa/edu/leetcode/Leetcode1496.java
@@ -0,0 +1,45 @@
+package cn.nuaa.edu.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author planb
+ * @date 2020/7/30 20:22
+ * 备注：1496. 判断路径是否相交
+ */
+public class Leetcode1496 {
+    public boolean isPathCrossing(String path) {
+        Map<Integer,ArrayList<Integer>> map = new HashMap<>();
+        ArrayList<Integer> li = new ArrayList<>();
+        li.add(0);
+        map.put(0,li);
+        int x = 0,y = 0;
+        char[] arr = path.toCharArray();
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] == 'N'){
+                y++;
+            }else if(arr[i] == 'S'){
+                y--;
+            }else if(arr[i] == 'W'){
+                x--;
+            }else {
+                x++;
+            }
+            ArrayList<Integer> list2 = new ArrayList<>();
+            if(map.containsKey(x)){
+                ArrayList<Integer> list = map.get(x);
+                if(list.contains(y)){
+                    return true;
+                }
+                list.add(y);
+                map.put(x,list);
+            }else{
+                list2.add(y);
+                map.put(x,list2);
+            }
+        }
+        return false;
+    }
+}
