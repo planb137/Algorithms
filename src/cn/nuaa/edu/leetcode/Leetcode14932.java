7e362dbe23f16dc7dd81ca23cf525d37af6533a2
planb137 <qiujj@nuaa.edu.cn>
2020-08-02 13:46:19 +0800

    leetcode1493

--- src/cn/nuaa/edu/leetcode/Leetcode14932.java
+++ src/cn/nuaa/edu/leetcode/Leetcode14932.java
@@ -9,23 +9,30 @@ public class Leetcode14932 {
     public int longestSubarray(int[] nums) {
         int k = nums.length;
         int res = 0;
-        for(int i = 0,j = i+1;i < k; i++){
+        for(int i = 0;i < k; i++){
             int z = 0;
-            if (nums[i] == 0) { z ++;}
-            while(z <= 1){
-                //z -= !nums[j++]
-                //int temp = nums
-                if(nums[++j] == 0 ) {
+            if(nums[i] == 0){
+                z++;
+            }
+            for (int j = i+1; j < k; j++) {
+                if(nums[j] == 0){
                     z++;
                 }
+                if (j == k-1){
+                    res = Math.max(res, j - i);
+                    break;
+                }
+                if (z == 2 ){
+                    res = Math.max(res, j - i -1);
+                    break;
+                }
             }
-            res = Math.max(res, j - i -1);
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,1,1,1,0,1,1,0,1};
+        int[] nums = {1,1,0,1};
         System.out.println(" === "+new Leetcode14932().longestSubarray(nums));
     }
 
