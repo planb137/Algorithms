fd49815253180f11e0e20f3687f76dc5d25d0645
planb137 <qiujj@nuaa.edu.cn>
2020-08-04 14:51:49 +0800

    KMP算法

---
+++ src/cn/nuaa/edu/KMPDemo.java
@@ -0,0 +1,15 @@
+package cn.nuaa.edu;
+
+/**
+ * @author planb
+ * @date 2020/8/4 14:14
+ * 备注：KMP算法
+ */
+public class KMPDemo {
+    void prefix(char[] pattern,int[] prefix,int n ){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
