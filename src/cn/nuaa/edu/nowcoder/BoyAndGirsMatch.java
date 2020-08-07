0e00a85c1fc70264d73af25b17a68032a67d49fa
planb137 <qiujj@nuaa.edu.cn>
2020-08-07 16:32:40 +0800

    匈牙利算法的最大男女匹配问题

---
+++ src/cn/nuaa/edu/nowcoder/BoyAndGirsMatch.java
@@ -0,0 +1,42 @@
+package cn.nuaa.edu.nowcoder;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+/**
+ * @author planb
+ * @date 2020/8/7 16:21
+ * 备注：匈牙利算法的最大男女匹配问题
+ */
+public class BoyAndGirsMatch {
+    int N = 50;
+    int[][] line = new int[N][N];
+    int match[] = new int[N];
+    boolean[] used;
+
+    boolean find(int x){
+        for (int i = 1; i <= 4; i++) {
+            if(line[x][i]==1 && !used[i]){
+                used[i] = true;
+                if(match[i] == 0 || find(match[i])){
+                    match[i] = x;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    int match(){
+        int sum = 0;
+        for (int i = 1; i <= 4; i++) {
+            int[] used = new int[N];
+            if(find(i)){
+                sum++;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+
+    }
+}
