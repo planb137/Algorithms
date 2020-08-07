67b81b1e32b913ae63878f93d51706800136ac63
planb137 <qiujj@nuaa.edu.cn>
2020-08-07 21:45:10 +0800

    nowcoder

---
+++ src/cn/nuaa/edu/nowcoder/Demo5.java
@@ -0,0 +1,40 @@
+package cn.nuaa.edu.nowcoder;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author planb
+ * @date 2020/8/7 19:03
+ * 备注：
+ */
+public class Demo5 {
+    public static void main(String[] args) throws IOException {
+       /* BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while ((input = br.readLine())!=null){
+
+        }
+        //BufferedReader br2 =  new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        int x = (int)d;
+        StringBuilder sb = new StringBuilder();
+        sb.append("人民币");
+        if (x == d ){
+
+        }*/
+        ArrayList<String> l1 = new ArrayList<String>();
+        ArrayList<Integer> l2 = new ArrayList<Integer>();
+        System.out.println(l1.getClass());
+        System.out.println(l2.getClass());
+        new Thread(() -> {
+
+        }).start();
+    }
+}
---
+++ src/cn/nuaa/edu/nowcoder/Demo6.java
@@ -0,0 +1,62 @@
+package cn.nuaa.edu.nowcoder;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author planb
+ * @date 2020/8/7 20:05
+ * 备注：
+ */
+public class Demo6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        while (sc.hasNext()){
+            String[] str = sc.nextLine().split(" ");
+            int n = Integer.parseInt(str[0]);
+            int m = Integer.parseInt(str[1]);
+            String[] arr = sc.nextLine().split(" ");
+            int [] num = new int[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                num[i] = Integer.parseInt(arr[i]);
+            }
+           // Arrays.sort(num);
+            int max  = 1;
+            int index = -1;
+            int index2 = -1;
+            int maxValue = num[0];
+            for (int i = num.length-1; i >=0 ; i--) {
+                maxValue = Math.max( num[i], maxValue);
+                if(m % num[i] == 0){
+                    max = Math.max(num[i], max);
+                    index = i;
+                    index2 = i;
+                }
+            }
+            if(max == 1){
+                for (int i = 0; i < num.length; i++) {
+                    if(num[i] == maxValue){
+                        sum += Math.abs(m-maxValue);
+                        maxValue = -1;
+                        continue;
+                    }
+                    sum += Math.abs(num[i]-1);
+                }
+            }else {
+                int temp = m / max;
+                for (int i = 0; i < num.length; i++) {
+                    if (i != index){
+                        sum += Math.abs(num[i]-temp);
+                        index = -1;
+                    }
+                    if(index2 != i){
+                        sum += (num[i]-1);
+                    }
+
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
