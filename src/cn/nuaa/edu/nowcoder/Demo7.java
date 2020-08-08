3d2fdc6289490697e593e9b95f4405d7234e2d30
planb137 <qiujj@nuaa.edu.cn>
2020-08-08 16:51:19 +0800

    nowcoder 整数素数合成个数

---
+++ src/cn/nuaa/edu/nowcoder/Demo7.java
@@ -0,0 +1,44 @@
+package cn.nuaa.edu.nowcoder;
+
+
+import java.util.Scanner;
+
+/**
+ * @author planb
+ * @date 2020/8/8 15:02
+ * 备注：
+ */
+public class Demo7 {
+    public static long count(long x) {
+        long sum = 0;
+        if (x <= 1) {
+            return 0;
+        } else if (x == 2 || x == 3) {
+            return 1;
+        } else {
+            if (x % 2 == 0) {
+                sum = x / 2;
+                return sum;
+            } else {
+                sum ++;
+                sum += (x - 3) / 2;
+                return sum;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] arr = new long[n];
+        for (int j = 0; j < n ; j++) {
+            arr[j] = sc.nextLong();
+        }
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += count(arr[i]);
+        }
+        System.out.println(sum);
+    }
+}
