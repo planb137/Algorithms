da63aae3f00e33a1be307d6e87bd2ec47496c08e
planb137 <qiujj@nuaa.edu.cn>
2020-08-10 14:46:02 +0800

    合法构成ip的数目

--- src/cn/nuaa/edu/nowcoder/Demo8.java
+++ src/cn/nuaa/edu/nowcoder/Demo8.java
@@ -8,12 +8,10 @@ import java.util.*;
  * 备注：nowcoder 字典排列
  */
 public class Demo8 {
-    public static int max(int[] x){
-        int max = -1;
-        for (int i = 0; i < x.length; i++) {
-            max = Math.max(max,x[i]);
-        }
-        return max;
+    public static int find(int[] x,int num){
+        int index = -1;
+
+        return index;
     }
 
     public static void main(String[] args) {
@@ -21,28 +19,24 @@ public class Demo8 {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[m];
-        LinkedList<Integer> list = new LinkedList<>();
-        ArrayList<Integer> li = new ArrayList<>();
+        int[] res = new int[n];
+
         HashSet<Integer> set = new HashSet<>();
         for (int j = 0; j < m; j++) {
             arr[j] = sc.nextInt();
             set.add(arr[j]);
-            li.add(arr[j]);
-            list.addLast(arr[j]);
+            res[j] = arr[j];
         }
         int pivot = arr[0];
-        Collections.sort(li);
+        String s = "asasas";
+        System.out.println(s.contains("adad"));
+
         for (int i = 1; i <= n; i++) {
-            if (set.contains(i)){
-                continue;
-            }else {
-                if (i<pivot){
-                    list.add(0, list.peekFirst());
-                }else {
-
-                }
-
-            }
+           if (set.contains(i)){
+               continue;
+           }else {
+               //find();
+           }
         }
     }
 }
