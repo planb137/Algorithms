65cd30ccd886eafc7ebdfade5d674d1b873a60ca
planb137 <qiujj@nuaa.edu.cn>
2020-08-14 12:52:38 +0800

    实现自己的AQS

---
+++ src/cn/nuaa/edu/juc/Sync.java
@@ -0,0 +1,36 @@
+package cn.nuaa.edu.juc;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author planb
+ * @date 2020/8/14 11:59
+ * 备注：实现自己的AQS
+ */
+public class Sync extends AbstractQueuedSynchronizer {
+    @Override
+    protected boolean tryAcquire(int arg) {
+        if(compareAndSetState(0, 1)){
+            //互斥锁
+            setExclusiveOwnerThread(Thread.currentThread());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean tryRelease(int arg) {
+       assert arg == 1;
+       if (!isHeldExclusively()) {
+           throw new IllegalMonitorStateException();
+       }
+       setExclusiveOwnerThread(null);
+       setState(0);
+       return true;
+    }
+
+    @Override
+    protected boolean isHeldExclusively() {
+        return getExclusiveOwnerThread() == Thread.currentThread();
+    }
+}
