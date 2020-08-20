b4d2245c43cf6eceed9123261dd1eec46c6ef702
planb137 <qiujj@nuaa.edu.cn>
2020-08-20 10:50:57 +0800

    lowbit （x & -x）: 返回x的最后一位1 例子：求二进制表示中1的个数

---
+++ src/cn/nuaa/edu/acwing/basic/LowBit.java
@@ -0,0 +1,21 @@
+package nuaa.edu.acwing.basic;
+
+/**
+ * @author planb
+ * @date 2020/8/20 10:40
+ * 备注：lowbit （x & -x）: 返回x的最后一位1 例子：求二进制表示中1的个数
+ */
+public class LowBit {
+    public static int lowBit(int x){
+        return  x & -x;
+    }
+    public static void main(String[] args) {
+        int n = 5 ;
+        int res = 0;
+        while ( n != 0){
+            n -= lowBit(n);
+            res++;
+        }
+        System.out.println(res);
+    }
+}
