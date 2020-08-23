249c61ad54fe75cfb83116e4c28b3bac7a8c8dfd
planb137 <qiujj@nuaa.edu.cn>
2020-08-23 21:06:52 +0800

    acwing 多重背包问题

---
+++ src/cn/nuaa/edu/acwing/basic/KnaspackMulti.java
@@ -0,0 +1,36 @@
+package nuaa.edu.acwing.basic;
+
+import java.util.Scanner;
+
+/**
+ * @author planb
+ * @date 2020/8/23 21:01
+ * 备注：acwing 多重背包问题
+ */
+public class KnaspackMulti {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] v = new int[n + 1];
+        int[] w = new int[n + 1];
+        int[] s = new int[n + 1];
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            v[i] = sc.nextInt();
+            w[i] = sc.nextInt();
+            s[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= s[i] && k * v[i] <= j; k++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - k * v[i]] + k * w[i]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][m]);;
+    }
+}
