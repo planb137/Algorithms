db98142732fbe37fd30e251c2ab0e16a8e61bb23
planb137 <qiujj@nuaa.edu.cn>
2020-08-25 21:36:46 +0800

    序列化对象

--- src/cn/nuaa/edu/test/Student.java
+++ src/cn/nuaa/edu/test/Student.java
@@ -8,6 +8,10 @@ import java.io.Serializable;
  * 备注：
  */
 public class Student implements Serializable {
+    //序列化对象版本控制，有关各版本反序列化时是否兼容。如果在新版本中这个值修改了，新版本就不兼容旧版本，
+    // 反序列化时会抛出InvalidClassException异常。如果修改较小，比如仅仅是增加了一个属性，
+    // 我们希望向下兼容，老版本的数据都能保留，那就不用修改；如果我们删除了一个属性，或者更改了类的继承关系，必然不兼容旧数据，
+    // 这时就应该手动更新版本号，即SerialVersionUid。
     private static final long seriaVersionId = 1L;
     private int age;
     private String name;
