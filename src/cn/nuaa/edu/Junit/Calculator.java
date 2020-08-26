8e0babfceb3eaae95dd91e79cfd0040f7395f9f4
planb137 <qiujj@nuaa.edu.cn>
2020-08-26 10:38:26 +0800

    Junit自动生成单元测试

---
+++ src/cn/nuaa/edu/Junit/Calculator.java
@@ -0,0 +1,24 @@
+package nuaa.edu.Junit;
+
+import org.junit.Test;
+
+/**
+ * @author planb
+ * @date 2020/8/26 10:24
+ * 备注：
+ */
+public class Calculator {
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int substract(int x, int y) {
+        return x - y;
+    }
+
+    public void print() {
+        System.out.println("planb");
+    }
+
+
+}
---
+++ src/cn/nuaa/edu/Junit/CalculatorsTest.java
@@ -0,0 +1,24 @@
+package nuaa.edu.Junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author planb
+ * @date 2020/8/26 10:35
+ * 备注：
+ */
+class CalculatorsTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void substract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void print() {
+    }
+}
