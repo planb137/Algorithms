8e663ed48d1c5e0f0cabcde09287315f7bc11cbb
planb137 <qiujj@nuaa.edu.cn>
2020-08-27 16:05:40 +0800

    可以到达所有点的最少点数目(入度为0的点)

--- src/cn/nuaa/edu/leetcode/LeetCode1557.java
+++ src/cn/nuaa/edu/leetcode/LeetCode1557.java
@@ -12,7 +12,8 @@ import java.util.List;
  * 备注：可以到达所有点的最少点数目(入度为0的点)
  */
 public class LeetCode1557 {
-    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+    // 通过65 / 66 个测试用例，100000个节点的时候超时
+    public List<Integer> findSmallestSetOfVertices2(int n, List<List<Integer>> edges) {
         List<Integer> res = new ArrayList<>();
         HashMap<Integer, Integer> map = new HashMap<>();
         for (List<Integer> list : edges) {
@@ -24,38 +25,52 @@ public class LeetCode1557 {
         }
         for (List<Integer> list : edges) {
             Integer x = list.get(0);
-            if(!map.containsKey(x) && !res.contains(x)){
+            if (!map.containsKey(x) && !res.contains(x)) {
                 res.add(x);
             }
         }
         return res;
     }
 
-   @Test
-   public void testFind(){
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        int[] degree = new int[n];
+        for (List<Integer> list : edges) {
+            degree[list.get(1)]++;
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n ; i++) {
+            if(degree[i] == 0){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void testFind() {
         int n = 6;
-       List<Integer> li = new ArrayList<>();
-       li.add(0);
-       li.add(1);
-       List<Integer> li2 = new ArrayList<>();
-       li2.add(0);
-       li2.add(2);
-       List<Integer> li3 = new ArrayList<>();
-       li3.add(2);
-       li3.add(5);
-       List<Integer> li4 = new ArrayList<>();
-       li4.add(3);
-       li4.add(4);
-       List<Integer> li5 = new ArrayList<>();
-       li5.add(4);
-       li5.add(2);
-       List<List<Integer>> edges =new ArrayList<>();
-       edges.add(li);
-       edges.add(li2);
-       edges.add(li3);
-       edges.add(li4);
-       edges.add(li5);
-       System.out.println(findSmallestSetOfVertices(n, edges).toString());
-   }
+        List<Integer> li = new ArrayList<>();
+        li.add(0);
+        li.add(1);
+        List<Integer> li2 = new ArrayList<>();
+        li2.add(0);
+        li2.add(2);
+        List<Integer> li3 = new ArrayList<>();
+        li3.add(2);
+        li3.add(5);
+        List<Integer> li4 = new ArrayList<>();
+        li4.add(3);
+        li4.add(4);
+        List<Integer> li5 = new ArrayList<>();
+        li5.add(4);
+        li5.add(2);
+        List<List<Integer>> edges = new ArrayList<>();
+        edges.add(li);
+        edges.add(li2);
+        edges.add(li3);
+        edges.add(li4);
+        edges.add(li5);
+        System.out.println(findSmallestSetOfVertices(n, edges).toString());
+    }
 
 }
