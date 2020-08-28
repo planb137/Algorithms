3543d1abad6915ad3b76e6c167d0c9e6b8fd2543
planb137 <qiujj@nuaa.edu.cn>
2020-08-28 14:51:34 +0800

    FrontPrefixSum acwing - 前缀和

--- src/cn/nuaa/edu/acwing/FrontPrefixSum.java
+++ src/cn/nuaa/edu/acwing/FrontPrefixSum.java
@@ -10,13 +10,15 @@ public class FrontPrefixSum {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         //int[] sum = new int[arr.length];
         //sum[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-           arr[i] += arr[i - 1];
+        int n = arr.length;
+        int[] s = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+           s[i] = s[i-1] + arr[i-1];
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int i = 0; i < s.length; i++) {
+            System.out.print(s[i] + " ");
         }
         System.out.println();
-        System.out.println(arr[5] - arr[2]);
+        System.out.println(s[3] - s[1]);
     }
 }
