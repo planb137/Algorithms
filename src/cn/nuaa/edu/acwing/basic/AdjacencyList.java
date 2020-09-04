972fd51bd2e38b917cacc0f496563ce99f509abd
planb137 <qiujj@nuaa.edu.cn>
2020-09-04 17:12:21 +0800

    链表的实现-用数组模拟单链表-邻接表

--- src/cn/nuaa/edu/acwing/basic/AdjacencyList.java
+++ src/cn/nuaa/edu/acwing/basic/AdjacencyList.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 /**
  * @author planb
  * @date 2020/9/4 16:33
- * 备注：邻接表的实现-用数组模拟（作用：存储图的树）
+ * 备注：链表的实现-用数组模拟单链表-邻接表（作用：存储图的树）
  */
 public class AdjacencyList {
     static Scanner in = new Scanner(System.in);
     static final int N = (int) 1e5 + 10;
     static int h[] = new int[N];//头结点的下标
     static int e[] = new int[N * 2]; //表示节点的值
-    static int ne[] = new int[N * 2]; //表示节点i next的指针
+    static int ne[] = new int[N * 2]; //表示节点i next的指针（存储的是下标）
     static int idx = 0; //当前已经用到了哪一个点的下标
 
     // 添加邻接表的边
