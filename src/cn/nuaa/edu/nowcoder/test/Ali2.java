c91d5341d240786b65158e2cc449d17836052390
planb137 <qiujj@nuaa.edu.cn>
2020-09-04 21:46:01 +0800

    阿里巴巴9月4号笔试  砍掉一个树枝后，求树的个数

--- src/cn/nuaa/edu/nowcoder/test/Ali2.java
+++ src/cn/nuaa/edu/nowcoder/test/Ali2.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * @author planb
  * @date 2020/9/4 09:26
- * 备注：阿里巴巴9月4号笔试  求树的个数
+ * 备注：阿里巴巴9月4号笔试  砍掉一个树枝后，求树的个数
  * 7
  * 1 2
  * 1 3
@@ -15,36 +15,64 @@ import java.util.*;
  * 3 7
  */
 public class Ali2 {
+    static int n;
+    static int[] h;
+    static int[] e;
+    static int[] ne;
+    static boolean[] used;
+    static int idx = 0;
+
+    public static void add(int a, int b) {
+        e[idx] = b;
+        ne[idx] = h[a];
+        h[a] = idx++;
+    }
+
+    public static void dfs(int x) {
+        used[x] = true;
+        for (int i = h[x]; i != -1; i = ne[x]) {
+            int j = e[i];
+            System.out.println(j + " ");
+            if (!used[j]) {
+                dfs(j);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int max = 4;
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        n = sc.nextInt();
+        //int max = 4;
+        h = new int[n + 1];
+        e = new int[2 * n];
+        ne = new int[2 * n];
+        used = new boolean[n + 1];
+        Arrays.fill(h, -1);
+
         for (int i = 0; i < n - 1; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            if (map.containsKey(a)) {
-                List<Integer> list = map.get(a);
-                list.add(b);
-                map.put(a, list);
-            } else {
-                List<Integer> li = new ArrayList<>();
-                li.add(b);
-                map.put(a, li);
-            }
+            add(a, b);
+            //add(b, a);
+        }
+        dfs(1);
+    }
+/*
+
+    //删除叶子节点到根节点的一条通路
+    public static void remove(Map<Integer, List<Integer>> map, int x, int[] sum) {
+        if (x == 1) {
+            return;
         }
 
-        //先找到所有叶子节点
-        Set<Integer> set = new HashSet<>();
         for (Integer in : map.keySet()) {
             List<Integer> aa = map.get(in);
-            for (Integer x : aa) {
-                if (!map.containsKey(x)) {
-                    set.add(x);
-                }
+            if (aa.contains(x)) {
+                sum[0]++;
             }
         }
-        System.out.println(max);
     }
+*/
 
 }
