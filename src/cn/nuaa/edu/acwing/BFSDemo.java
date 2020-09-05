8f83fe9f77fde4ab952d0e410cb1ea33953bf8fb
planb137 <qiujj@nuaa.edu.cn>
2020-09-05 10:35:14 +0800

    bfs 模板

---
+++ src/cn/nuaa/edu/acwing/BFSDemo.java
@@ -0,0 +1,42 @@
+package nuaa.edu.acwing;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * @author planb
+ * @date 2020/9/5 10:28
+ * 备注：bfs 模板
+ */
+public class BFSDemo {
+    static int n;
+    static int[] h;
+    static int[] e;
+    static int[] ne;
+    static boolean[] st;
+    static int idx = 0;
+
+    static LinkedList<Integer> q = new LinkedList<>();
+
+    static void bfs() {
+        while (!q.isEmpty()) {
+            int t = q.peekFirst();
+            q.pollFirst();
+
+            for (int i = h[t]; i != -1; i = ne[i]) {
+                int j = e[i];
+                if (!st[j]) {
+                    st[j] = true; // 表示点j已经被遍历过
+                    q.addLast(j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bfs();
+    }
+}
+
