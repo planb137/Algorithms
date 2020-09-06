95c1d90c5943ede897161593648852c2ec89f8e2
planb137 <qiujj@nuaa.edu.cn>
2020-09-06 22:00:09 +0800

    2020/9/6 华为笔试

---
+++ src/cn/nuaa/edu/nowcoder/huawei/Main11.java
@@ -0,0 +1,38 @@
+package nuaa.edu.nowcoder.huawei;
+
+import java.util.Scanner;
+
+/**
+ * @author planb
+ * @date 2020/9/6 19:39
+ * 备注：
+ */
+public class Main11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toUpperCase();
+        String[] s = str.split(";");
+        String[] s0 =s[0].split(" ");
+        String[] s1 =s[1].split(" ");
+        // int n = s.length;
+        char[] end = s[1].toCharArray();
+        int dot = 0;
+        for (int i = 0; i < end.length; i++) {
+            if (end[i] == ',' || end[i] == '.' || end[i] == '?') {
+                dot++;
+            }
+        }
+        int num = s1.length + dot;
+        int dif= 0;
+        int j = 0;
+        for (int i = 0; i < s0.length-1; i++) {
+            if (s0[i].equals(s1[j])){
+                continue;
+            }else {
+                dif+=2;
+            }
+            j++;
+        }
+        System.out.println("("+dif+","+num+")");
+    }
+}
