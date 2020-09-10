95c4384dd21711f0316c48b0b8e0e87e8e0f02d9
planb137 <qiujj@nuaa.edu.cn>
2020-09-10 19:47:56 +0800

    leetcode 72. 编辑距离

---
+++ src/cn/nuaa/edu/leetcode/LeetCode72.java
@@ -0,0 +1,39 @@
+package nuaa.edu.leetcode;
+
+/**
+ * @author planb
+ * @date 2020/9/10 19:36
+ * 备注：leetcode 72. 编辑距离
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * 你可以对一个单词进行如下三种操作：
+ *
+ *     插入一个字符
+ *     删除一个字符
+ *     替换一个字符
+ *
+ */
+public class LeetCode72 {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+}
