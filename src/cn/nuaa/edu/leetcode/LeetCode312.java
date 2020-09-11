84122c07cf1642e349f023a2b549455aaafcc36b
planb137 <qiujj@nuaa.edu.cn>
2020-09-11 14:04:06 +0800

    leetcode 312. 戳气球

---
+++ src/cn/nuaa/edu/leetcode/LeetCode312.java
@@ -0,0 +1,36 @@
+package nuaa.edu.leetcode;
+
+/**
+ * @author planb
+ * @date 2020/9/11 13:48
+ * 备注：leetcode 312. 戳气球
+ * 有 n 个气球，编号为0 到 n-1，每个气球上都标有一个数字，这些数字存在数组 nums 中。
+ *
+ * 现在要求你戳破所有的气球。如果你戳破气球 i ，就可以获得 nums[left] * nums[i] * nums[right] 个硬币。
+ * 这里的 left 和 right 代表和 i 相邻的两个气球的序号。注意当你戳破了气球 i 后，气球 left 和气球 right 就变成了相邻的气球。
+ *
+ * 求所能获得硬币的最大数量。
+
+ */
+public class LeetCode312 {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] a = new int[n + 2];
+        a[0] = 1;
+        a[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            a[i] = nums[i - 1];
+        }
+        int[][] f = new int[n + 2][n + 2];
+        for (int len = 3; len <= n + 2; len++) {
+            for (int i = 0; i + len - 1 <= n + 1; i++) {
+                int j = i + len - 1;
+                for (int k = i + 1; k < j; k++) {
+                    f[i][j] = Math.max(f[i][j], f[i][k] + f[k][j] + a[i] * a[j] * a[k]);
+                }
+            }
+        }
+        return f[0][n + 1];
+    }
+
+}
