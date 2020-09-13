69c3383e419ecb0030c6682e9b2875c0e1551249
planb137 <qiujj@nuaa.edu.cn>
2020-09-13 19:36:10 +0800

    test

---
+++ src/cn/nuaa/edu/test/DemoTest2.java
@@ -0,0 +1,17 @@
+package nuaa.edu.test;
+
+import java.util.ArrayList;
+
+/**
+ * @author planb
+ * @date 2020/9/13 19:33
+ * 备注：
+ */
+public class DemoTest2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list.toString());
+    }
+}
