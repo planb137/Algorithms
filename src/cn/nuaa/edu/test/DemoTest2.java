a13d147974df478e635742ae9b16f86b9d431c39
planb137 <qiujj@nuaa.edu.cn>
2020-09-14 16:26:47 +0800

    系统研发工程师-云原生方向

--- src/cn/nuaa/edu/test/DemoTest2.java
+++ src/cn/nuaa/edu/test/DemoTest2.java
@@ -1,18 +1,36 @@
 package nuaa.edu.test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author planb
- * @date 2020/9/13 19:33
- * 备注：
+ * @date 2020/9/1 15:00
+ * 备注：快手（系统研发工程师-云原生方向）面试题：复制带随机指针的链表
  */
+class Node {
+    Node ran;
+    Node next;
+    int value;
+}
 public class DemoTest2 {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        
-        System.out.println(list.toString());
+        Map<Node,Node> map = new HashMap<>();
+        Node newNode = new Node();
+        Node head = new Node();
+        newNode = head;
+        while(head.next != null){
+            Node temp = head.next;
+            Node pivot = head.ran;
+            map.put(temp,pivot);
+            head = head.next;
+        }
+        while(newNode.next!=null){
+            if(map.containsKey(newNode)){
+                Node va = map.get(newNode);
+                newNode.ran = va;
+            }
+        }
+        System.out.println("Hello World!");
     }
 }
