2195bee5e09f37fd390062c6efc4cb4f0f036a57
planb137 <qiujj@nuaa.edu.cn>
2020-09-18 15:36:52 +0800

    test

--- src/cn/nuaa/edu/test/Test2.java
+++ src/cn/nuaa/edu/test/Test2.java
@@ -1,7 +1,9 @@
 package nuaa.edu.test;
 
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author planb
@@ -32,10 +34,40 @@ public class Test2 {
 //        }
 //
 //        long sds = 0;
-        BigInteger bg = new BigInteger("1");
+/*        BigInteger bg = new BigInteger("1");
         BigInteger bigInteger = bg.add(new BigInteger("2"));
         Integer i = Integer.parseInt(bigInteger.toString());
         System.out.println(i.getClass().toString());//class java.lang.Integer
         System.out.println(bg.getClass().toString());//class java.math.BigInteger
+        BigDecimal bg2 = new BigDecimal(1212);*/
+/*
+        LinkedHashMap<String,Integer> map = new LinkedHashMap<>();
+        map.put("bb",8);
+        map.put("aa",2);
+        map.put("cc",5);
+        map.put("ee",3);
+        map.put("ff",1);
+        for (String s: map.keySet()) {
+            System.out.println(s+" "+map.get(s));
+        }*/
+        Map<Integer, Integer> tree = new HashMap<>();
+        tree.put(2, 1);
+        tree.put(3, 1);
+        tree.put(1, 1);
+        tree.put(4, 1);
+        for (Integer in : tree.keySet()) {
+            System.out.println(in);
+        }
+
+        // HashMap<String, Integer> map2 = new HashMap<>();
+/*        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);*/
+/*        for (int j = 1; j <=3 ; j++) {
+            map2.put(String.valueOf(j), j);
+        }
+        for (String s :map2.keySet()) {
+            System.out.println(map2.get(s));
+        }*/
     }
 }
